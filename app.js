c3827ec725c4f4895b6b3d60f2f103ae90d8f6ec
Utlamo <danielkasiurak1@gmail.com>
2022-09-04 15:27:48 +0200

    Add manifest and app.js as main app file

---
+++ app.js
@@ -0,0 +1,4 @@
+//const headerList = document.getElementsByTagName("h1");
+//const header = headerList[0];
+//header.textContent = "Title from addon";
+document.getElementsByTagName("h1")[0].textContent = "Hello World from addon";
